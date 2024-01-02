872f30b1c68cb7d862c22c5a62c3abdba7ed4f0c
Subash Limbu <subashlimbu077@gmail.com>
2024-01-02 16:19:33 +0545

    deployed page in github

--- module2.mjs
+++
@@ -1,5 +0,0 @@
-import ui,{a,b} from "./module1.mjs";
-
-console.log(ui);
-console.log(a);
-console.log(b);
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,53 +1,53 @@
-import React from "react";
-import { useState } from "react";
-import "../App.css";
-import About from "./About";
-import Navbar from "./Navbar";
-import Textform from "./Textform";
-import Alert from "./Alert";
+// import React from "react";
+// import { useState } from "react";
+// import "../App.css";
+// import About from "./About";
+// import Navbar from "./Navbar";
+// import Textform from "./Textform";
+// import Alert from "./Alert";
 
-export default function Home() {
-  let [mode, setMode] = useState("light");
-  let [alert, setAlert] = useState(null);
-  const [colour, setColour] = useState("white");
+// export default function Home() {
+//   let [mode, setMode] = useState("light");
+//   let [alert, setAlert] = useState(null);
+//   const [colour, setColour] = useState("white");
 
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 2000);
-  };
+//   const showAlert = (message, type) => {
+//     setAlert({
+//       msg: message,
+//       type: type,
+//     });
+//     setTimeout(() => {
+//       setAlert(null);
+//     }, 2000);
+//   };
 
-  const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "rgb(34 41 48)";
-      showAlert("Dark mode has been enabled.", "success");
-      document.title = "TextUtils - Dark Mode";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enabled.", "success");
-      document.title = "TextUtils - Light Mode";
-    }
-  };
-  return (
-    <>
-      <Navbar
-        title={"TextUtils"}
-        mode={mode}
-        colour={colour}
-        setColour={setColour}
-        toggleMode={toggleMode}
-      />
-      <div className="container my-3">
-        <Alert alert={alert} />
-        <Textform mode={mode} colour={colour} showAlert={showAlert} />
-      </div>
-      <About />
-    </>
-  );
-}
+//   const toggleMode = () => {
+//     if (mode === "light") {
+//       setMode("dark");
+//       document.body.style.backgroundColor = "rgb(34 41 48)";
+//       showAlert("Dark mode has been enabled.", "success");
+//       document.title = "TextUtils - Dark Mode";
+//     } else {
+//       setMode("light");
+//       document.body.style.backgroundColor = "white";
+//       showAlert("Light mode has been enabled.", "success");
+//       document.title = "TextUtils - Light Mode";
+//     }
+//   };
+//   return (
+//     <>
+//       <Navbar
+//         title={"TextUtils"}
+//         mode={mode}
+//         colour={colour}
+//         setColour={setColour}
+//         toggleMode={toggleMode}
+//       />
+//       <div className="container my-3">
+//         <Alert alert={alert} />
+//         <Textform mode={mode} colour={colour} showAlert={showAlert} />
+//       </div>
+//       <About />
+//     </>
+//   );
+// }
